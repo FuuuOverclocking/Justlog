07c4625d8f6fcecc77c3b66f2e2b5bad49472bcb
Fuu <g.for.xyz@gmail.com>
2022-05-15 15:04:35 +0800

    feat(justlog-cli): 增加用 VS Code 打开功能

--- packages/justlog-cli/src/actions/new-blog.ts
+++ packages/justlog-cli/src/actions/new-blog.ts
@@ -1,13 +1,16 @@
+import readline from 'readline';
+import { spawnSync } from 'child_process';
 import path from 'path';
 import fs from 'fs-extra';
 import { v4 as uuidv4 } from 'uuid';
 import { settings } from '../settings';
 import debug from '../utils/debug';
-import { packagePaths } from 'shared';
+import chalk from 'chalk';
+import { packagePaths, pathBackslashToSlash } from 'shared';
 
 export function newBlog(name: string): void {
     try {
-        const blogDir = path.resolve(settings.blogRootDir, name);
+        const blogDir = pathBackslashToSlash(path.resolve(settings.blogRootDir, name));
         check(name, blogDir);
 
         const files = {
@@ -21,7 +24,10 @@ export function newBlog(name: string): void {
             ),
         };
 
-        files['article.tsx'] = files['article.tsx'].replace(/%JUSTLOG_DIR%/g, packagePaths.justlog);
+        files['article.tsx'] = files['article.tsx'].replace(
+            /%JUSTLOG_DIR%/g,
+            packagePaths.justlog,
+        );
         files['article.md'] = files['article.md'].replace(/%BLOG_UUID%/g, uuidv4());
 
         fs.writeFileSync(
@@ -35,7 +41,20 @@ export function newBlog(name: string): void {
             'utf-8',
         );
 
-        debug.info(`已在目录 ${blogDir} 下创建博客的模板文件.`);
+        debug.info(`已在目录 ${blogDir} 下创建博客模板.`);
+        debug.info(`按下按键:`);
+        debug.info(
+            `    [${chalk.underline(' Enter ')}]     = 用 VS Code 打开`,
+        );
+        debug.info(`    [${chalk.underline(' Any other ')}] = 退出`);
+
+        readline.emitKeypressEvents(process.stdin);
+        if (process.stdin.isTTY) process.stdin.setRawMode(true);
+        process.stdin.on('keypress', (str, key) => {
+            if (key.name !== 'return') process.exit(0);
+            spawnSync('code', [blogDir], { shell: true });
+            process.exit(0);
+        });
     } catch (e) {
         debug.panic(`未能在 ${settings.blogRootDir} 下创建博客 ${name}.`);
     }
